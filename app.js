6f502ba797844567b870856e957071a9c01f9c5d
ouliana <ouliana.kotik@gmail.com>
2023-10-12 13:22:52 +0300

    undo cors

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-app.use(cors)
+// app.use(cors)
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
